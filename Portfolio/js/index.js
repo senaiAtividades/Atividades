4cbd89b9a7177d4e56408c829073a16e409205bb
senaiAtividades <alvaro.l.gomes@aluno.senai.br>
2024-10-16 11:03:36 -0300

    Update index.js

--- Portfolio/js/index.js
+++ Portfolio/js/index.js
@@ -3,7 +3,7 @@ const menuContato = document.querySelector("#menuContato");
 const divContato = document.querySelector("#divContato");
 const inputNome = document.querySelector("#inputNome");
 const inputEmail = document.querySelector("#inputEmail");
-const inputSenha = document.querySelector("#inputSenha");
+const inputMensagem = document.querySelector("#inputMensagem");
 
 menuContato.addEventListener("click", () => {
     divContato.style.display = 'block'
@@ -14,12 +14,35 @@ menuContato.addEventListener("click", () => {
 })
 
 const fecharForm = () => {
-    inputNome.value = ""
-    inputEmail.value = ""
-    inputSenha.value = ""
-    divContato.style.display = 'none'
+    inputNome.value = "";
+    inputEmail.value = "";
+    inputMensagem.value = "";
+    divContato.style.display = 'none';
     divContato.style.opacity = 0;
 }
+
+const btnContato = document.querySelector("#btnContato");
+
+btnContato.addEventListener("click", () => {
+    const nome = inputNome.value.trim();
+    const email = inputEmail.value.trim();
+    const mensagem = inputMensagem.value.trim();
+    
+    if (!nome || !email || !mensagem) {
+        alert("Por favor, preencha todos os campos!");
+        return;
+    }
+    
+    const remetente = encodeURIComponent(`Mensagem de ${nome}`);
+    const conteudo = encodeURIComponent(`Nome: ${nome}\nEmail para Contato: ${email}\nMensagem: \n${mensagem}`);
+    
+    const mailtoLink = `mailto:alvaro.l.gomes@aluno.senai.br?subject=${remetente}&body=${conteudo}`;
+    
+    window.location.href = mailtoLink;
+
+    fecharForm();
+});
+
 
 // mudar cores background
 const bckColor = document.querySelector("#bckColor");
